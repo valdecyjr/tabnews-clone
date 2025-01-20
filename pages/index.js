b2785206c7e6b303bee656a29dcabd99f0424179
valdecyjr <valdecysjunior@gmail.com>
2025-01-20 19:47:27 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-export default function main()
-{
-    return <main>
-        <h1>pagina inicial</h1>
+export default function main() {
+  return (
+    <main>
+      <h1>pagina inicial</h1>
     </main>
+  );
 }
