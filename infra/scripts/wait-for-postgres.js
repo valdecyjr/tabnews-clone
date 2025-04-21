74f02f8344035d542055a334bc2330840c3c2bf1
valdecyjr <valdecysjunior@gmail.com>
2025-04-21 07:11:37 -0300

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,28 @@
+const { exec } = require("node:child_process")
+
+function checkPostgres(){
+    exec('docker exec postgres-dev pg_isready', handleReturn)
+
+    function handleReturn(err, stdout, stderr) {
+        if (err) {
+            if(stderr == "") {
+                setTimeout( checkPostgres, 5000);
+                return;
+            }
+            console.error(`Erro ao verificar o Postgres: ${stderr}`);
+            setTimeout(checkPostgres, 5000);
+            return;
+        }
+
+        if (stdout.includes("accepting connections")) {
+            console.log("Postgres está aceitando conexões.");
+            return;
+        }
+
+        console.log("Postgres ainda não está aceitando conexões. Tentando novamente...");
+        setTimeout(checkPostgres, 5000);
+    }
+}
+
+console.log(" Aguardando o Postgres aceitar conexões...");
+checkPostgres();
