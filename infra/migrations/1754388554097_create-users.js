3ad22a9ea9292b6416b7403a07cb2b940170ba50
valdecyjr <valdecysjunior@gmail.com>
2025-08-05 07:41:39 -0300

    feat: add migration to create `users` table

---
+++ infra/migrations/1754388554097_create-users.js
@@ -0,0 +1,39 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primarykey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    // For reference, GitHub username is limited to 39 characters
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 254 in length?, https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 60 in length?, https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+    // Why timestamp with timezone?, https://justatheory.com/2012/04/postgres-use-timestamptz/
+    create_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+    update_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('UTC', now())"),
+    },
+  });
+};
+
+exports.down = false;
